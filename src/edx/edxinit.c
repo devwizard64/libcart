82b0278b854dc29fa1b7fe072090ece5676270f6
devwizard64 <devwizard64@gmail.com>
2025-08-07 20:57:29 -0700

    EDX detection fix

--- src/edx/edxinit.c
+++ src/edx/edxinit.c
@@ -10,14 +10,17 @@ int edx_init(void)
 {
 	__cart_acs_get();
 	__cart_wr(EDX_KEY_REG, EDX_KEY);
-	__cart_wr(EDX_SYS_CFG_REG, EDX_CFG_SDRAM_OFF);
-	/* Check bootloader ROM label */
-	__cart_dma_rd(__cart_buf, 0x10000020, 20);
-	if (memcmp(__cart_buf, edx_bootx, 20) && memcmp(__cart_buf, edx_boot3, 12))
+	if (__cart_rd(EDX_EDID_REG) >> 16 != 0xED64)
 	{
-		CART_ABORT();
+		/* Check bootloader ROM label */
+		__cart_wr(EDX_SYS_CFG_REG, EDX_CFG_SDRAM_OFF);
+		__cart_dma_rd(__cart_buf, 0x10000020, 20);
+		__cart_wr(EDX_SYS_CFG_REG, EDX_CFG_SDRAM_ON);
+		if (
+			memcmp(__cart_buf, edx_bootx, 20) &&
+			memcmp(__cart_buf, edx_boot3, 12)
+		) CART_ABORT();
 	}
-	__cart_wr(EDX_SYS_CFG_REG, EDX_CFG_SDRAM_ON);
 	__cart_dom1 = 0x80370C04;
 	cart_size = 0x4000000; /* 64 MiB */
 	__cart_acs_rel();
